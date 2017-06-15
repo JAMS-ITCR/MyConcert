70fd37a8e446207a9247ecbbd761b107528cdc21
Malcolm Davis <me@malcolmdavis.xyz>
2017-06-15 00:48:21 -0600

    Basic login(no auth) implemented.

--- site/js/controllers/login.controller.js
+++ site/js/controllers/login.controller.js
@@ -1,40 +1,47 @@
 angular
 .module('app')
 .controller('loginController', loginController);
-loginController.$inject = ['$scope'];
- function loginController ($scope) {
+loginController.$inject = ['$scope','$http','$window'];
+ function loginController ($scope,$http,$window) {
    $scope.userName = "";
    $scope.passWord = "";
-   $scope.response = "";
-   $scope.count = 0;
+   if(JSON.parse(sessionStorage.user) &&  JSON.parse(sessionStorage.priv)){
+     $window.location.href = '/#!/home';
+   }
    $scope.log = function(userName, passWord) {
         var Url = "http://myconcertv2.cloudapp.net/UserService.svc/login/"+userName+"/"+passWord;
-        var xhttp = new XMLHttpRequest();
-        xhttp.open("GET", Url, true);
-        xhttp.setRequestHeader("Content-type", "application/json");
-        xhttp.send();
-        var response = JSON.parse(xhttp.responseText);
-        /*switch(response) {
-            case 103:
-                code block
+        $http({
+          method: 'GET',
+          url: Url
+        }).then (
+          function (data) {
+            sessionStorage.user = JSON.stringify($scope.userName);
+            switch (data.data.loginResult) {
+              case 103:
+                sessionStorage.priv = JSON.stringify("admin");
+                $window.location.href = '/#!/home';
                 break;
-            case 104:
-                code block
+              case 104:
+                sessionStorage.priv = JSON.stringify("fan");
+                $window.location.href = '/#!/home';
                 break;
-            case 105:
-                code block
+              case 105:
+                sessionStorage.user = JSON.stringify("");
+                $scope.msj = "El usuario ya está activo, cierre sesión en el otro dispositivo.";
                 break;
-            case 106:
-                code block
+              case 106:
+                sessionStorage.user = JSON.stringify("");
+                $scope.msj = "El usuario y contraseña no coinciden.";
                 break;
-            default:
-                code block
-                break;
-        }*/
-        $scope.response = response;
-        if (  $scope.response == ""){
-          $scope.response = "not cool bro";
-        }
+              default:
+
+            }
+          },
+          function (error){
+            console.error(data);
+          }
+
+        );
         $scope.count += 1;
     };
 };
