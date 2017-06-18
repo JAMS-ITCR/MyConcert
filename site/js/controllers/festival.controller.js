669f3c7469bf64787ad5a0257e2b107deb9f4fbe
santigr17 <santigr17@gmail.com>
2017-06-18 10:53:21 -0600

    Tabla dentro del acordion

--- site/js/controllers/festival.controller.js
+++ site/js/controllers/festival.controller.js
@@ -4,13 +4,14 @@ angular
 festivalController.inject = ['$scope'];
 festivalController.inject = ['$http'];
 festivalController.inject = ['$stateParams'];
+festivalController.inject = ['$location'];
 
 var urlgetBanda = "http://myconcertv2.cloudapp.net/PEDS/BandsService.svc/bands/";//MAS ID
 var urlgetAllFestivals = "http://myconcertv2.cloudapp.net/PEDS/BillboardServices.svc/festivals";
 var urlgetPais = "http://myconcertv2.cloudapp.net/CountriesService.svc/countries";
 var urlgetGenero = "http://myconcertv2.cloudapp.net//PEDS/BandsService.svc/genders?idgenero="//Mas ID Género
 
-function festivalController($scope,$http,$stateParams) {
+function festivalController($scope,$http,$stateParams,$location) {
 
   $scope.Festival={};
   $scope.festivalID=$stateParams.festivalID;
@@ -67,19 +68,30 @@ function festivalController($scope,$http,$stateParams) {
           }
       },function myerror (response){
         $scope.info = "Request fallido "+ response.statusText;
+
       });
   }
 
   setValues=function(RawValues){
-    for (var i = 0; i < RawValues.length; i++) {
-      if(RawValues[i].IdFestival==$scope.festivalID){
-        $scope.Festival=RawValues[i];
+    var indice=0;
+    console.log(RawValues);
+    while (indice<RawValues.length) {
+      if(RawValues[indice].IdFestival==$scope.festivalID){
+        $scope.Festival=RawValues[indice];
         break;
       }
+      indice++;
     }
-    getPais($scope.Festival.IdPais);
-    console.log("Buscando la banda: "+$scope.Festival.IdBanda);
-    getInfoReco($scope.Festival.IdBanda);
+    console.log(indice);
+    if(indice>=RawValues.length){
+      var newUrl = "/404/";
+      $location.url(newUrl);
+    }
+    else {
+          getPais($scope.Festival.IdPais);
+          console.log("Buscando la banda: "+$scope.Festival.IdBanda);
+          getInfoReco($scope.Festival.IdBanda);
+    }
   }
   getInfo();
 }
